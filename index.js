aa58a8d9531878a779e9a40982d17f413f059c2f
GOKUL-KANNAN-P <gokulkannanp.cse2021@citchennai.net>
2024-02-25 11:28:24 +0530

    avien

--- index.js
+++ index.js
@@ -1,59 +1,58 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-const bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'Gokul@003',
-    database: 'screening',
+    user: 'avnadmin',
+    host: 'mysql-39af648c-gokul.a.aivencloud.com',
+    password: 'AVNS_5W135YZrjuwuLR-WHt5',
+    database: 'library',
 });
 
+app.post('/register', (req, res) => {
+    const {
+        firstname,
+        lastname,
+        dob,
+        age,
+        gender,
+        email,
+        contact,
+        wages,
+        country,
+        job
+    } = req.body;
 
-app.post('/register',(req,res)=>{
-    const firstname = req.body?.firstname;
-    const lastname = req.body?.lastname;
-    const dob = req.body?.dob;
-    const age = req.body?.age;
-    const gender = req.body?.gender;
-    const email = req.body?.email;
-    const backupemail = req.body?.backupemail;
-    const contact = req.body?.contact;
-    const address = req.body?.address;
-    const ncompany = req.body?.ncompany;
-    const companyname = req.body?.companyname;
-    const wages = req.body?.wages;
-    const resume = req.body?.resume;
-    const country = req.body?.country;
-    const job = req.body?.job;
-    const adopt = req.body?.adopt;
-
-    console.log(req.body);
-
-    db.query('INSERT INTO register (firstname,lastname,dob,age,gender,email,backupemail,contact,address,ncompany,companyname,wages,resume,country,job,adopt) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
-        [firstname,lastname,dob,age,gender,email,backupemail,contact,address,ncompany,companyname,wages,resume,country,job,adopt]
-    )
-
-})
-
-app.get('/employeelist' , (req,res) => {
-    db.query("SELECT * FROM register", 
-    (err,result)=>{
-        if(err)
-        {
-            console.log(err);
+    db.query('INSERT INTO register (firstname, lastname, dob, age, gender, email, contact, wages, country, job) VALUES (?,?,?,?,?,?,?,?,?,?)',
+        [firstname, lastname, dob, age, gender, email, contact, wages, country, job],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            } else {
+                res.status(200).json({ insertId: result.insertId });
+            }
         }
-        else{
-            res.send(result);
+    );
+});
+
+app.get('/employeelist', (req, res) => {
+    db.query("SELECT * FROM register",
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            } else {
+                res.send(result);
+            }
         }
-    }
-    )
-})
+    );
+});
 
 app.put("/updateEmployee/:id", (req, res) => {
     const id = req.params.id;
@@ -64,35 +63,65 @@ app.put("/updateEmployee/:id", (req, res) => {
         country: req.body?.country,
         contact: req.body?.contact,
         wages: req.body?.wages,
-        job: req.body?.job,
-      };
-  
+        job: req.body?.job
+    };
+
     db.query(
-      "UPDATE register SET ? WHERE id = ?",
-      [updatedEmployee, id],
-      (err, result) => {
-        if (err) {
-          console.log(err);
-          res.status(500).send("Error updating employee");
-        } else {
-          res.send(result);
+        "UPDATE register SET ? WHERE id = ?",
+        [updatedEmployee, id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send("Error updating employee");
+            } else {
+                res.send(result);
+            }
         }
-      }
     );
+});
+app.post('/update', (req, res) => {
+  const {
+      address,
+      degree,
+      college,
+      twelth,
+      tenth
+  } = req.body;
+
+  db.query("SELECT id FROM register ORDER BY id DESC LIMIT 1", (err, result) => {
+      if (err) {
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+      } else {
+          const lastEmployeeId = result[0].id;
+          db.query(
+              "UPDATE register SET address = ?, degree = ?, college = ?, twelth = ?, tenth = ? WHERE id = ?",
+              [address, degree, college, twelth, tenth, lastEmployeeId],
+              (err, result) => {
+                  if (err) {
+                      console.log(err);
+                      res.status(500).send('Internal Server Error');
+                  } else {
+                      res.status(200).json({ message: 'Employee details updated successfully' });
+                  }
+              }
+          );
+      }
   });
-  
+});
+
 
 app.delete("/delete/:id", (req, res) => {
     const id = req.params.id;
     db.query("DELETE FROM register WHERE id = ?", id, (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(result);
-      }
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(result);
+        }
     });
-  });
-
+});
 
 app.listen(5000, () => {
     console.log('Server started');
